c50812e580b3df26c65ca694ccc44dd02570a8d9
Zach Short <zach.short@fantomworks.com>
2024-01-11 17:02:38 -0500

    modified personal .env file and fixed missing links with port, port is functionaing now on local machine

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+require('dotenv').config();
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.DB_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
