b34b9f896eb7fb8695a20cee6c0209d723e6025e
Zach Short <zach.short@fantomworks.com>
2024-01-15 16:25:09 -0500

    boilerplate handlebars for all page

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.get('/gallery', (req, res) => {
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('http://localhost:3001'));
 });
