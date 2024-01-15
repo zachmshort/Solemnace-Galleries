2aff7d939974a8d0fabdcfe8ec27e7bc23981007
Zach Short <zach.short@fantomworks.com>
2024-01-15 15:44:16 -0500

    wrote script for isolating buttons on hover but commented out

--- public/js/script.js
+++ public/js/script.js
@@ -1,101 +1,114 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var navContainer = document.querySelector('.navContainer');
-    navContainer.addEventListener('click', function () {
-      navContainer.classList.toggle('active');
+  var navContainer = document.querySelector('.navContainer');
+  navContainer.addEventListener('click', function () {
+    navContainer.classList.toggle('active');
 
-      var dropdownItems = document.querySelector('.dropdown-items');
-      var navImgDropDown = document.querySelector('.navImgDropDown');
+    var dropdownItems = document.querySelector('.dropdown-items');
+    var navImgDropDown = document.querySelector('.navImgDropDown');
 
-      if (navContainer.classList.contains('active')) {
-        fadeIn(dropdownItems);
-        fadeIn(navImgDropDown);
-      } else {
-        fadeOut(dropdownItems);
-        fadeOut(navImgDropDown);
-      }
-    });
-
-    function fadeIn(element) {
-      element.style.display = 'block';
-      element.style.opacity = 0;
-
-      var opacity = 0;
-      var interval = setInterval(function () {
-        if (opacity < 1) {
-          opacity += 0.125;
-          element.style.opacity = opacity;
-        } else {
-          clearInterval(interval);
-        }
-      }, 100);
-    }
-
-    function fadeOut(element) {
-      var opacity = 1;
-      var interval = setInterval(function () {
-        if (opacity > 0) {
-          opacity -= 0.1;
-          element.style.opacity = opacity;
-        } else {
-          element.style.display = 'none';
-          clearInterval(interval);
-        }
-      }, 100);
+    if (navContainer.classList.contains('active')) {
+      fadeIn(dropdownItems);
+      fadeIn(navImgDropDown);
+    } else {
+      fadeOut(dropdownItems);
+      fadeOut(navImgDropDown);
     }
   });
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('.galleryDiv ul li a');
-    const page = document.querySelector('.galleryPage');
-    const mainImage = document.querySelector('.mainImage');
-    let currentFaction = ''
-    
-    links.forEach(link => {
-    link.addEventListener('mouseover', function () {
-        currentFaction = link.classList[0];
-        console.log('Current category:', currentFaction);
-        page.style.backgroundColor = getColor(currentFaction);
-        changeImage(currentFaction);
-    });
+  function fadeIn(element) {
+    element.style.display = 'block';
+    element.style.opacity = 0;
 
-    link.addEventListener('mouseout', function () {
-        page.style.backgroundColor = '#000';
-    });
+    var opacity = 0;
+    var interval = setInterval(function () {
+      if (opacity < 1) {
+        opacity += 0.125;
+        element.style.opacity = opacity;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+
+  function fadeOut(element) {
+    var opacity = 1;
+    var interval = setInterval(function () {
+      if (opacity > 0) {
+        opacity -= 0.1;
+        element.style.opacity = opacity;
+      } else {
+        element.style.display = 'none';
+        clearInterval(interval);
+      }
+    }, 100);
+  }
 });
 
-    function getColor(category) {
-        switch (category) {
-          case 'xenos':
-            return '#AF7D83'; 
-        case 'chaos':
-            return '#827F79'; 
-        case 'spaceMarines':
-            return '#BF9B30'; 
-        case 'imperialArmies':
-            return '#677990'; 
-        case 'necron':
-            return '#698174'; 
-        case 'aeldari':
-            return '#7A6E8D'; 
-            default:
-                return '#000'; 
-        }
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.galleryDiv ul li a');
+  const page = document.querySelector('.galleryPage');
+  const mainImage = document.querySelector('.mainImage');
+  let currentFaction = ''
+  
+  links.forEach(link => {
+  link.addEventListener('mouseover', function () {
+      currentFaction = link.classList[0];
+      page.style.backgroundColor = getColor(currentFaction);
+      changeImage(currentFaction);
+      // hideOtherItems(link);
+  });
 
-    function changeImage(category) {
-      const categoryToImagePath = {
-        'xenos': '/images/xenos.png',
-        'chaos': '/images/chaos.png',
-        'spaceMarines': '/images/spacemarines.png',
-        'imperialArmies': '/images/imperialarmies.png',
-        'necron': '/images/necron.png',
-        'aeldari': '/images/aeldari.png',
-      };
-
-      if (categoryToImagePath.hasOwnProperty(category)) {
-        const imagePath = categoryToImagePath[category];
-        mainImage.src = imagePath;
-      } 
-    }
+  link.addEventListener('mouseout', function () {
+      page.style.backgroundColor = '#000';
+      // showAllItems();
+  });
 });
 
+  function getColor(category) {
+      switch (category) {
+        case 'xenos':
+          return '#AF7D83'; 
+      case 'chaos':
+          return '#827F79'; 
+      case 'spaceMarines':
+          return '#BF9B30'; 
+      case 'imperialArmies':
+          return '#677990'; 
+      case 'necron':
+          return '#698174'; 
+      case 'aeldari':
+          return '#7A6E8D'; 
+          default:
+              return '#000'; 
+      }
+  }
+
+  function changeImage(category) {
+    const categoryToImagePath = {
+      'xenos': '/images/xenos.png',
+      'chaos': '/images/chaos.png',
+      'spaceMarines': '/images/spacemarines.png',
+      'imperialArmies': '/images/imperialarmies.png',
+      'necron': '/images/necron.png',
+      'aeldari': '/images/aeldari.png',
+    };
+
+    if (categoryToImagePath.hasOwnProperty(category)) {
+      const imagePath = categoryToImagePath[category];
+      mainImage.src = imagePath;
+    } 
+  }
+  // function hideOtherItems(selectedLink) {
+  //   links.forEach(link => {
+  //     if (link !== selectedLink) {
+  //       link.style.opacity = '0';
+  //     }
+  //   });
+  // }
+
+  // function showAllItems() {
+  //   links.forEach(link => {
+  //     link.style.opacity = '1';
+  //   });
+  // }
+});
