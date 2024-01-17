7f9a16d2e66af3bf80c8726b163f4a77248945bb
macrowil <macrow.co@gmail.com>
2024-01-17 13:28:56 -0700

    fixed routing and server.js

--- server.js
+++ server.js
@@ -33,45 +33,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/gallery", (req, res) => {
-  res.render("gallery");
-});
-
-// app.get('/all', async (req, res) => {
-//   try {
-//     const figurineData = await Figurine.findAll();
-
-//     res.render('all', { figurines: figurineData });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// app.get("/xenos", (req, res) => {
-//   res.render("xenos"); // , { figurine/figurines?? }
-// });
-
-// app.get('/necron', (req, res) => {
-//   res.render('necron'); // , { figurine/figurines?? }
-// });
-
-// app.get("/chaos", (req, res) => {
-//   res.render("chaos"); // , { figurine/figurines?? }
-// });
-
-// app.get("/imperial-armies", (req, res) => {
-//   res.render("imperialarmies"); // , { figurine/figurines?? }
-// });
-
-// app.get("/space-marines", (req, res) => {
-//   res.render("spacemarines"); // , { figurine/figurines?? }
-// });
-
-// app.get("/aeldari", (req, res) => {
-//   res.render("aeldari"); // , { figurine/figurines?? }
-// });
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
